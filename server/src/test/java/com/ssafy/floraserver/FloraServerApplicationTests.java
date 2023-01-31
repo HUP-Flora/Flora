e2110cf18ea749033a1e5b156f6c9465b64e53a9
yeomyuri42@gmail.com <yeomyuri42@gmail.com>
2023-01-31 13:02:51 +0900

    feat: 백 초기 환경 세팅

---
+++ server/src/test/java/com/ssafy/floraserver/FloraServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.floraserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloraServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
