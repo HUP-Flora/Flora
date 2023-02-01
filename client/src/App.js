2b8ec422dd1d0873ff0997bd5ff593fefeb53dd2
HeeJeYang <ghjadmb21@gmail.com>
2023-02-01 10:11:29 +0900

    feat: 라우터 설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,14 @@
 import AppRouter from "./components/AppRouter";
-import { Route, Routes } from "react-router-dom";
 import { PhoneStatusHeader } from "./components/common/PhoneStatusHeader";
-import { KakaoPayment } from "./pages/KakaoPayment";
-import { KakaoPaymentSuccess } from "./pages/KakaoPaymentSuccess";
 import { AppContainer } from "./styles/common/AppStyle";
 
 function App() {
-  return (
-    <AppContainer>
-      <AppRouter />
-      <PhoneStatusHeader />
-      <Routes>
-        <Route path="/kakao-payment" element={<KakaoPayment />} />
-        <Route
-          path="/kakao-payment/success"
-          element={<KakaoPaymentSuccess />}
-        />
-      </Routes>
-    </AppContainer>
-  );
+	return (
+		<AppContainer>
+			<PhoneStatusHeader />
+			<AppRouter />
+		</AppContainer>
+	);
 }
 
 export default App;
--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { KakaoPayment } from "./pages/KakaoPayment";
+import { KakaoPaymentSuccess } from "./pages/KakaoPaymentSuccess";
 
 import Flolive from "./pages/flolive/Flolive";
 
@@ -7,6 +9,8 @@ function AppRouter() {
 		<Router>
 			<Routes>
 				<Route path="/flolive" element={<Flolive />} />
+				<Route path="/kakao-payment" element={<KakaoPayment />} />
+				<Route path="/kakao-payment/success" element={<KakaoPaymentSuccess />} />
 			</Routes>
 		</Router>
 	);
