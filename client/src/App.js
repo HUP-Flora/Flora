4f881eb039c4cbdac7ed9f0e6b4e25b4b5cdffe3
HeeJeYang <ghjadmb21@gmail.com>
2023-02-01 10:12:38 +0900

    feat: 라우터 설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import AppRouter from "./components/AppRouter";
-import { PhoneStatusHeader } from "./components/common/PhoneStatusHeader";
 import { AppContainer } from "./styles/common/AppStyle";
 
 function App() {
 	return (
 		<AppContainer>
-			<PhoneStatusHeader />
 			<AppRouter />
 		</AppContainer>
 	);
