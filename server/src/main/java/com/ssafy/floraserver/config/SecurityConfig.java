6e19246e075c612eec2a5c022095c51a118baca8
yeomyuri42@gmail.com <yeomyuri42@gmail.com>
2023-02-03 13:45:53 +0900

    feat: SecurityConfig 수정 및 CORS 설정

--- server/src/main/java/com/ssafy/floraserver/config/SecurityConfig.java
+++ server/src/main/java/com/ssafy/floraserver/config/SecurityConfig.java
@@ -4,7 +4,7 @@ import com.ssafy.floraserver.common.auth.CustomOAuth2UserService;
 import com.ssafy.floraserver.common.auth.OAuth2LoginSuccessHandler;
 import com.ssafy.floraserver.common.jwt.JwtAccessDeniedHandler;
 import com.ssafy.floraserver.common.jwt.JwtAuthenticationEntryPoint;
-import com.ssafy.floraserver.common.jwt.TokenProvider;
+import com.ssafy.floraserver.common.jwt.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
@@ -16,7 +16,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -25,8 +27,8 @@ public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
     private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
-    private final TokenProvider tokenProvider;
-    private final CorsFilter corsFilter;
+
+    private final JwtFilter jwtFilter;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
@@ -39,14 +41,23 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .httpBasic().disable()
+                .formLogin().disable() // FormLogin 사용하지않음
 
+                // CORS 허용 설정
+                .cors().configurationSource(corsConfigurationSource())
+
+                // URL 권한 설정
+                .and()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/users").authenticated()
+                .antMatchers(HttpMethod.GET, "/auth/users").authenticated()
                 .anyRequest().permitAll()
 
+                // JWT 설정
                 .and()
-                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
 
+                // 예외 설정
                 .exceptionHandling()
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
                 .accessDeniedHandler(jwtAccessDeniedHandler)
@@ -56,15 +67,29 @@ public class SecurityConfig {
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
-                .and()
-                .formLogin().disable() // FormLogin 사용하지않음
-
                 // 소셜 로그인 설정
+                .and()
                 .oauth2Login()
-                .successHandler(oAuth2LoginSuccessHandler)//동의하고 계속하기를 눌렀을때,
+                .successHandler(oAuth2LoginSuccessHandler)// 동의하고 계속하기를 눌렀을때,
                 .userInfoEndpoint()
-                .userService(customOAuth2UserService);//userService 설정
+                .userService(customOAuth2UserService);// userService 설정
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*"); // 허용할 URL
+        config.addAllowedHeader("*"); // 허용할 Header
+        config.addAllowedMethod("*"); // 허용할 Http Method
+
+        source.registerCorsConfiguration("*", config);
+
+        return source;
     }
 }
