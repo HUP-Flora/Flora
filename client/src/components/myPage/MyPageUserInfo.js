1267d7ff8f0fd68558f901e88e38db9e84e67fde
aeebbr <aeebbr@gmail.com>
2023-02-06 04:34:06 +0900

    feat: 마이페이지 회원 정보 컴포넌트 수정

--- client/src/components/myPage/MyPageUserInfo.js
+++ client/src/components/myPage/MyPageUserInfo.js
@@ -1,20 +1,35 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import { BoldText, GrayText, ShadowCard } from "../../styles/common/CommonStyle";
 import { ListContiner, ListHeader } from "../../styles/myPage/MyPageStyle";
 import { ShadowCardContent } from "../../styles/myPage/MyPageUserInfoStyle";
 
 function MyPageUserInfo(props) {
+	const navigate = useNavigate();
+
+	const handleLogout = () => {
+		// (백) 로그아웃
+		// navigate("/");
+	};
+
+	const handleClickUserDelete = () => {
+		navigate("/userDelete");
+	};
+
 	return (
 		<ListContiner paddingBottom="112">
 			<ListHeader>
 				<BoldText size="19">회원 정보</BoldText>
-				<GrayText weight="bold">더보기</GrayText>
 			</ListHeader>
 			<ShadowCard padding="24">
 				<ShadowCardContent>
-					<BoldText size="19">로그아웃</BoldText>
-					<BoldText size="19">회원탈퇴</BoldText>
+					<BoldText size="19" onClick={handleLogout}>
+						로그아웃
+					</BoldText>
+					<BoldText size="19" onClick={handleClickUserDelete}>
+						회원탈퇴
+					</BoldText>
 				</ShadowCardContent>
 			</ShadowCard>
 		</ListContiner>
