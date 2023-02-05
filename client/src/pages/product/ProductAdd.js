4a4d93794b8e94195e5a02a88af6f0a01b1bd8c6
aeebbr <aeebbr@gmail.com>
2023-02-05 09:25:26 +0900

    feat: Product 폴더 구조, 파일명 수정

--- client/src/pages/product/ProductAdd.js
+++ client/src/pages/product/ProductAdd.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 import StatusBar from "../../components/common/StatusBar";
-import ProductForm from "../../components/productAdd/ProductForm";
-import ProductAddBottomButtons from "../../components/productAdd/ProductAddBottomButtons";
+import ProductForm from "../../components/product/ProductForm";
+import ProductAddBottomButtons from "../../components/product/ProductBottomButtons";
 
 function ProductAdd(props) {
 	return (
