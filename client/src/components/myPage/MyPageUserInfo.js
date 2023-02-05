5c9c04b331b576da0d6d9fc99e0a1b298fc24386
aeebbr <aeebbr@gmail.com>
2023-02-05 16:50:45 +0900

    feat: Mypage 회원 정보 컴포넌트 마크업

---
+++ client/src/components/myPage/MyPageUserInfo.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { BoldText, GrayText, ShadowCard } from "../../styles/common/CommonStyle";
+import { ListContiner, ListHeader } from "../../styles/myPage/MyPageStyle";
+import { ShadowCardContent } from "../../styles/myPage/MyPageUserInfoStyle";
+
+function MyPageUserInfo(props) {
+	return (
+		<ListContiner paddingBottom="112">
+			<ListHeader>
+				<BoldText size="19">회원 정보</BoldText>
+				<GrayText weight="bold">더보기</GrayText>
+			</ListHeader>
+			<ShadowCard padding="24">
+				<ShadowCardContent>
+					<BoldText size="19">로그아웃</BoldText>
+					<BoldText size="19">회원탈퇴</BoldText>
+				</ShadowCardContent>
+			</ShadowCard>
+		</ListContiner>
+	);
+}
+
+export default MyPageUserInfo;
