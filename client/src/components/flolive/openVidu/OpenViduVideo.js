4ed877cd3d2a3063d7dd7bb80f1e3abf992f3002
류원창 <wom05169@gmail.com>
2023-02-06 17:48:16 +0900

    feat: 플로라이브 name, session상태 커스텀 훅 테스트 중

--- client/src/components/flolive/openVidu/OpenViduVideo.js
+++ client/src/components/flolive/openVidu/OpenViduVideo.js
@@ -15,18 +15,22 @@ import {
 } from "../../../styles/openViduStyle";
 import { useRecoilValue } from "recoil";
 import { LmyNameState, LmySessionIdState } from "../../../recoil/flolive";
+import { useFlolive } from "../../../hooks/use-flolive";
 
 const APPLICATION_SERVER_URL =
 	process.env.NODE_ENV === "production" ? "" : "https://demos.openvidu.io/";
 
 class OpenViduVideo extends Component {
 	// 플로라이브 테스트 코드
+
 	constructor(props) {
 		super(props);
 
+		this.useflo = useFlolive();
+
 		// These properties are in the state's component in order to re-render the HTML whenever their values change
 		this.state = {
-			mySessionId: "커스텀훅 예정",
+			mySessionId: this.useflo.Lname,
 			myUserName: "커스텀훅 예정",
 			session: "커스텀훅 예정",
 			mainStreamManager: undefined, // Main video of the page. Will be the 'publisher' or one of the 'subscribers'
---
+++ client/src/recoil/flolive.js
@@ -0,0 +1,12 @@
+import { atom } from "recoil";
+
+export const LmyNameState = atom({
+	key: "LmyNameState",
+	default: "",
+});
+
+export const LmySessionIdState = atom({
+	key: "LmySessionIdState",
+	default: "",
+});
+
