fb9e1d519496b8e0fd4e4efbc125ce512234cc63
aeebbr <aeebbr@gmail.com>
2023-02-07 23:42:01 +0900

    feat: flolive 종료 모달 구현 및 마크업

--- client/src/pages/flolive/FloliveTest.js
+++ client/src/pages/flolive/FloliveTest.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 import OpenVidu from "../../components/floliveTest/openVidu/OpenViduVideo";
 import registerServiceWorker from "../../components/floliveTest/openVidu/registerServiceWorker";
 import Chat from "../../components/chattingTest/Chatting";
+import ModalContainer from "../../components/floliveTest/openVidu/OpenViduExitModal";
 
 function Flolive(props) {
+	const [isModalShow, setIsModalShow] = useState(false);
+
 	registerServiceWorker();
 
 	return (
 		<div style={{ height: "100vh" }}>
-			<OpenVidu />
+			<OpenVidu isModalShow={isModalShow} setIsModalShow={setIsModalShow} />
 			<Chat />
+			{isModalShow && <ModalContainer isModalShow={isModalShow} setIsModalShow={setIsModalShow} />}
 		</div>
 	);
 }
