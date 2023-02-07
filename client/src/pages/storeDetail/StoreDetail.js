079244c70c88ec94cb72a70c40f6c48f7ff66523
류원창 <wom05169@gmail.com>
2023-02-07 17:23:57 +0900

    feat: 예약 페이지 백엔드 연동 상태 관리 테스트

--- client/src/pages/storeDetail/StoreDetail.js
+++ client/src/pages/storeDetail/StoreDetail.js
@@ -4,8 +4,20 @@ import StatusBar from "../../components/common/StatusBar";
 import StoreInfo from "../../components/storeDetail/StoreInfo";
 import StoreDetailButtons from "../../components/storeDetail/StoreDetailButtons";
 import StoreTabs from "../../components/storeDetail/StoreTabs";
+import { RstoreIdState } from "../../recoil/reservation";
+import { useSetRecoilState } from "recoil";
 
-function StoreDetail(props) {
+function StoreDetail() {
+	// 예약 페이지로 넘겨줄 storeId가 필요하다.
+	// 가게 상세페이지로 넘어오면, storeId를 저장한다.
+	const setRstoreId = useSetRecoilState(RstoreIdState);
+
+	// 페이지 로드 시, storeId를 저장한다.
+	// useEffect(() => {
+	// 	setRstoreId(props.match.params.storeId);
+	// }, []);
+
+
 	return (
 		<>
 			<StatusBar text="가게 상세" />
