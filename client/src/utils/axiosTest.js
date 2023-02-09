25cee2c82cee06cc34f29686fa8d4b48ad03884e
류원창 <wom05169@gmail.com>
2023-02-09 15:06:16 +0900

    feat: axios 공통 api 함수 작성

--- client/src/utils/axiosTest.js
+++ client/src/utils/axiosTest.js
@@ -1,16 +1,46 @@
-import axios from 'axios';
-
-const request = axios.create({
-	baseURL: 'https://dog.ceo/api/breeds/image/',
-	timeout: 1000,
-});
-
-request.interceptors.response.use(
-	(response) => {
-		return response;
-	},
-	(error) => {
-		console.log(error);
-		return Promise.reject(error);
-	}
-);
+// import axios from 'axios';
+//
+// const token = localStorage.getItem('flora-token');
+//
+// export const request = axios.create({
+//
+// 	baseURL: 'https://dog.ceo/api/breeds/image/',
+// 	timeout: 1000,
+// 	headers: {
+// 		Authorization: `Bearer ${token}`,
+// 	}
+// });
+//
+// import axios from 'axios';
+//
+// const getDog = (url, data) {
+// 	axios({
+// 		method: 'get',
+// 		url: url,,
+// 		data: data,
+// 	})
+// 	.then(res => {
+// 		console.log(res);
+// 	})
+// }
+//
+// import request from "";
+//
+// const getDog = (url, data) {
+// 	request({
+// 		method: 'get',
+// 		url: url,,
+// 		data: data,
+// 		header: {header: localStorage.get("TOKEN")}
+// 	}
+// }
+//
+// request.interceptors.response.use(
+// 	(response) => {
+// 		return response;
+// 	},
+// 	(error) => {
+// 		console.log(error);
+// 		return Promise.reject(error);
+// 	}
+// );
