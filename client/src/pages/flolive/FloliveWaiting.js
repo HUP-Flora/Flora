39e19c72039b2f3cc8b47f81d1ae9fd477734b82
류원창 <wom05169@gmail.com>
2023-02-10 17:14:37 +0900

    feat: 예약 관련 백엔드 연동 테스트 중

--- client/src/pages/flolive/FloliveWaiting.js
+++ client/src/pages/flolive/FloliveWaiting.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import FloLiveWaitingContainer from "../../components/flolive/FloLiveWaitingContainer";
+import useStroeDetail from "../../hooks/useStroeDetail";
+import { LliveStatusState, LorderNumberState } from "../../recoil/flolive";
+import { useRecoilValue } from "recoil";
+import { useNavigate } from "react-router-dom";
 
-function FloliveWaiting(props) {
+function FloliveWaiting() {
+	const LliveStatus = useRecoilValue(LliveStatusState);
+	const LorderNumber = useRecoilValue(LorderNumberState);
+	const { checkLiveStatusAPI } = useStroeDetail();
+
+	useEffect(() => {
+		const checkStatusInterval = setInterval(() => {
+			// 5초마다 라이브 상태를 확인한다.
+			checkLiveStatusAPI(LorderNumber);
+			// console.log("checkLiveStatusAPI");
+		}, 5000);
+		return () => {
+			clearInterval(checkStatusInterval);
+		};
+	}, [LorderNumber]);
+
+	const navigate = useNavigate();
+	useEffect(() => {
+		console.log("LliveStatus", LliveStatus);
+		// 라이브 신청이 수락하면 작동할 로직.
+		// if (LliveStatus === "live") {
+		// navigate("/flolive");
+	}, [LliveStatus]);
+
 	return <FloLiveWaitingContainer />;
 }
 
