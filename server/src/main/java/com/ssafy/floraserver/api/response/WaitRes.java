29eea6523849f41088b98b1ce398c4fdf6050785
이희진 <oliqoeiilo@naver.com>
2023-02-11 10:42:32 +0900

    feat: 플로라이브 response dto 생성

---
+++ server/src/main/java/com/ssafy/floraserver/api/response/WaitRes.java
@@ -0,0 +1,37 @@
+package com.ssafy.floraserver.api.response;
+
+import com.ssafy.floraserver.db.entity.Order;
+import com.ssafy.floraserver.db.entity.TimeUnit;
+import com.ssafy.floraserver.db.entity.enums.Role;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class WaitRes {
+
+    private Role userRole;
+    private Long oId;
+    private LocalDate reservationDate;
+    private Long reservationTime;
+    private String sName;
+    private String pName;
+
+    @Builder
+    public WaitRes(Order order) {
+        this.userRole = order.getUId().getRole();
+        this.oId = order.getOId();
+        this.reservationDate = order.getConId().getReservationDate();
+        this.reservationTime = order.getConId().getReservationTime().getTuId();
+        this.sName = order.getSId().getName();
+        this.pName = order.getPId().getName();
+    }
+}
+
+
