5a149bcd22262dc314c4f471062248d38ea0cc21
yeomyuri42@gmail.com <yeomyuri42@gmail.com>
2023-02-12 20:11:16 +0900

    fix: 상품, 리뷰 soft delete 적용

--- server/src/main/java/com/ssafy/floraserver/api/service/ReviewService.java
+++ server/src/main/java/com/ssafy/floraserver/api/service/ReviewService.java
@@ -4,10 +4,7 @@ import com.ssafy.floraserver.api.request.ReviewReq;
 import com.ssafy.floraserver.api.response.StoreReviewRes;
 import com.ssafy.floraserver.api.response.UserReviewRes;
 import com.ssafy.floraserver.api.vo.FileVO;
-import com.ssafy.floraserver.db.entity.Order;
-import com.ssafy.floraserver.db.entity.Review;
-import com.ssafy.floraserver.db.entity.Store;
-import com.ssafy.floraserver.db.entity.User;
+import com.ssafy.floraserver.db.entity.*;
 import com.ssafy.floraserver.db.repository.OrderRepository;
 import com.ssafy.floraserver.db.repository.ReviewRepository;
 import com.ssafy.floraserver.db.repository.StoreRepository;
@@ -91,5 +88,15 @@ public class ReviewService {
                 .build());
 
         order.setReviewflag(true);
+    }
+
+    public void deleteReview(Long revId, Map<String, String> authInfo) {
+        Long uId = Long.parseLong(authInfo.get("uId"));
+
+        // 삭제할 리뷰
+        Review review = reviewRepository.findById(revId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        review.deleteReview();
     }
 }
--- server/src/main/java/com/ssafy/floraserver/db/repository/ProductRepository.java
+++ server/src/main/java/com/ssafy/floraserver/db/repository/ProductRepository.java
@@ -7,8 +7,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    @Query("select distinct p from Product p where p.sId.sId = :sId")
+    @Query("select distinct p from Product p where p.pId = :pId and p.softDelete = false")
+    Optional<Product> findById(Long pId);
+
+    @Query("select distinct p from Product p where p.sId.sId = :sId and p.softDelete = false")
     Page<Product> findAllBySId(@Param("sId") Long sId, Pageable pageable);
 }
