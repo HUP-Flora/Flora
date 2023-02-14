ef5bc088bed615b7f4db4190002cc23ae17d637b
yeomyuri42@gmail.com <yeomyuri42@gmail.com>
2023-02-14 09:54:31 +0900

    feat: JWT 예외처리

--- server/src/main/java/com/ssafy/floraserver/common/exception/ErrorCode.java
+++ server/src/main/java/com/ssafy/floraserver/common/exception/ErrorCode.java
@@ -29,7 +29,7 @@ public enum ErrorCode {
     CONFERENCE_ENTRY_NOT_ALLOWED(HttpStatus.FORBIDDEN, "입장할 수 없는 화상미팅입니다."),
     REPORT_EXISTS(HttpStatus.FORBIDDEN, "이미 신고된 주문입니다."),
     REFRESH_NOT_VALID(HttpStatus.UNAUTHORIZED, "리프레시 토큰이 유효하지 않습니다."),
-    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "액세스 토큰이 만료되었습니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "액세스 토큰 기한이 만료되었습니다."),
     WRONG_TYPE_TOKEN(HttpStatus.UNAUTHORIZED, "토큰의 타입이 잘못되었습니다."),
     UNSUPPORTED_TOKEN(HttpStatus.UNAUTHORIZED, "지원하지 않는 토큰입니다."),
     WRONG_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 토큰입니다."),
--- server/src/main/java/com/ssafy/floraserver/common/jwt/JwtExceptionFilter.java
+++ server/src/main/java/com/ssafy/floraserver/common/jwt/JwtExceptionFilter.java
@@ -1,7 +1,11 @@
 package com.ssafy.floraserver.common.jwt;
 
-import io.jsonwebtoken.JwtException;
-import org.springframework.http.HttpStatus;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssafy.floraserver.common.exception.ErrorCode;
+import io.jsonwebtoken.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -10,23 +14,46 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
+@Slf4j
+@RequiredArgsConstructor
 @Component
 public class JwtExceptionFilter extends OncePerRequestFilter {
+
+    private final ObjectMapper objectMapper;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        try{
-            filterChain.doFilter(request, response);
-        }catch (JwtException e){
-            setErrorResponse(HttpStatus.UNAUTHORIZED, response, e);
+        response.setCharacterEncoding("utf-8");
+
+        try {
+            filterChain.doFilter(request, response); // JwtAuthenticationFilter로 이동
+        } catch (ExpiredJwtException e) {
+            log.info("만료된 토큰입니다.");
+            setErrorResponse(request, response, ErrorCode.EXPIRED_TOKEN, e);
+        } catch (UnsupportedJwtException e) {
+            log.info("지원되지 않는 토큰입니다.");
+            setErrorResponse(request, response, ErrorCode.UNSUPPORTED_TOKEN, e);
+        } catch (Exception e) {
+            log.info("토큰이 잘못되었습니다.");
+            setErrorResponse(request, response, ErrorCode.WRONG_TOKEN, e);
         }
     }
 
-    public void setErrorResponse(HttpStatus status, HttpServletResponse response, Throwable ex) throws IOException {
-        response.setStatus(status.value());
-        response.setContentType("application/json; charset=UTF-8");
+    public void setErrorResponse(HttpServletRequest req, HttpServletResponse res, ErrorCode code, Throwable ex) throws IOException {
 
-        JwtExceptionResponse jwtExceptionResponse = new JwtExceptionResponse(ex.getMessage(), HttpStatus.UNAUTHORIZED);
-        response.getWriter().write(jwtExceptionResponse.convertToJson());
+        res.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        final Map<String, Object> body = new HashMap<>();
+        body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+        body.put("error", code.name());
+        body.put("message", ex.getMessage());
+        body.put("path", req.getServletPath());
+        final ObjectMapper mapper = new ObjectMapper();
+        res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        mapper.writeValue(res.getOutputStream(), body);
+
     }
 }
