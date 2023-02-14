9b01ab4f7ea6170d54562ec7fc62a32d5e5b994c
ryuwc <wom05169@gmail.com>
2023-02-14 22:52:10 +0900

    feat: 소켓 서버 https 작업

--- signaling/router.js
+++ signaling/router.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send({ response: "I am alive" }).status(200)
+  res.send({ response: "사람살려.." }).status(200)
 })
 
 module.exports = router
