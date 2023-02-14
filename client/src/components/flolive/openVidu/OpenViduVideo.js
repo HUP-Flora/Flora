5dbb969539a9b51114c7e5415cde3a36f2884d55
aeebbr <aeebbr@gmail.com>
2023-02-14 13:08:06 +0900

    fix: 오픈비두 해결 시도07

--- client/src/components/flolive/openVidu/OpenViduVideo.js
+++ client/src/components/flolive/openVidu/OpenViduVideo.js
@@ -377,35 +377,46 @@ class OpenViduVideo extends Component {
 	}
 
 	async createSession(sessionId) {
-		// const encodedString = Buffer.from("OPENVIDUAPP:" + process.env.REACT_APP_OPENVIDU_SERVER_SECRET).toString(
-		// 	"base64"
-		// );
+		const response = await axios
+			.post(
+				APPLICATION_SERVER_URL + "openvidu/api/sessions",
+				{ customSessionId: sessionId },
+				{
+					headers: {
+						// Authorization: `Basic T1BFTlZJRFVBUFAgOiBNWV9TRUNSRVQ=`,
+						Authorization:
+							"Basic " + window.btoa("OPENVIDUAPP:" + process.env.REACT_APP_OPENVIDU_SERVER_SECRET),
+						// Authorization: "Basic " + encodedString,
+
+						// Authorization: `Basic EncodeBase64(OPENVIDUAPP:MY_SECRET)`,
+						"Content-Type": "application/json",
+					},
+				}
+			)
+			.then(response => {
+				console.log("세션 생성", response.data);
+				console.log("세션 아이디", response.data.sessionId);
+			})
+			.catch(error => {
+				console.log("세션 생성 에러", error);
+			});
+
+		console.log(response);
+
+		return response?.data?.sessionId; // The sessionId
+	}
+
+	async createToken(sessionId) {
+		console.log("토큰 생성", sessionId);
 		const response = await axios.post(
-			APPLICATION_SERVER_URL + "openvidu/api/sessions",
-			{ customSessionId: sessionId },
+			APPLICATION_SERVER_URL + "openvidu/api/sessions/" + sessionId + "/connection",
+			{},
 			{
 				headers: {
-					// Authorization: `Basic T1BFTlZJRFVBUFAgOiBNWV9TRUNSRVQ=`,
-					'Authorization': "Basic " + window.btoa("OPENVIDUAPP:" + process.env.REACT_APP_OPENVIDU_SERVER_SECRET),
-					// Authorization: "Basic " + encodedString,
-					
-					// Authorization: `Basic EncodeBase64(OPENVIDUAPP:MY_SECRET)`,
+					Authorization:
+						"Basic " + window.btoa("OPENVIDUAPP:" + process.env.REACT_APP_OPENVIDU_SERVER_SECRET),
 					"Content-Type": "application/json",
 				},
-			}
-			);
-			return response.data; // The sessionId
-		}
-		
-		async createToken(sessionId) {
-			const response = await axios.post(
-				APPLICATION_SERVER_URL + "openvidu/api/sessions/" + sessionId + "/connection",
-				{},
-				{
-					headers: { 
-						'Authorization': "Basic " + window.btoa("OPENVIDUAPP:" + process.env.REACT_APP_OPENVIDU_SERVER_SECRET),
-						"Content-Type": "application/json"
-					 },
 			}
 		);
 		return response.data; // The token
