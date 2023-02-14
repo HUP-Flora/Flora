a47842023a5d1a004c719ddd8da43beb83bcebe1
HeeJeYang <ghjadmb21@gmail.com>
2023-02-15 02:07:22 +0900

    feat: 플로라이브 테스트 진행 중

--- client/src/hooks/useReservationAcceptApi.js
+++ client/src/hooks/useReservationAcceptApi.js
@@ -1,15 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import api from "../utils/api";
 
-
-
 export const useReservationAcceptApi = () => {
+	const navigate = useNavigate();
+
 	const reservationAcceptApi = async oId => {
 		await api({
 			method: "GET",
 			url: `flolive/${oId}`,
 		})
 			.then(response => {
-				console.log(response);
+				// console.log(response);
+				const sessionId = response.data.sessionId;
+				navigate(`flolive/${oId}/${sessionId}`);
 			})
 			.catch(error => {
 				console.log("예약 수락 에러", error);
