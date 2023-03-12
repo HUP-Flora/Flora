2abdbc60bba842368a9c35611253fa7bc821f869
HeeJeYang <ghjadmb21@gmail.com>
2023-03-12 11:56:58 +0900

    chore: 사진 업로드 컴포넌트 주석 추가

--- client/src/components/store/UploadPicture.js
+++ client/src/components/store/UploadPicture.js
@@ -1,3 +1,10 @@
+import { useCallback, useRef } from "react";
+import { useRecoilState, useSetRecoilState } from "recoil";
+
+// States
+import { storeImageFileState, storeImagePreviewState } from "../../recoil/signup";
+
+// Styles
 import {
 	ImageInput,
 	PicturePreview,
@@ -5,15 +12,14 @@ import {
 	UploadPictureSection,
 	PictureSection,
 } from "../../styles/common/CommonStyle";
+
+// Images
 import EmptyPhoto from "../../assets/photo/Photo.png";
 import PlusButton from "../../assets/photo/PlusButton.png";
-import { useCallback, useRef } from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { storeImageFileState, storeImagePreviewState } from "../../recoil/signup";
 
 export function UploadPicture() {
-	const [imageFile, setImageFile] = useRecoilState(storeImageFileState);
 	const [imagePreview, setImagePreview] = useRecoilState(storeImagePreviewState);
+	const setImageFile = useSetRecoilState(storeImageFileState);
 	const imageRef = useRef();
 
 	// // 기존 이미지 삽입
@@ -24,13 +30,18 @@ export function UploadPicture() {
 	const onUploadImage = useCallback(() => {
 		const file = imageRef.current.files[0];
 		const reader = new FileReader();
+
+		// 이미지 파일 저장
 		setImageFile(file);
+
+		// 이미지 미리보기 저장 (recoil 안쓰고 useState만으로 가능하지 않을까?)
 		reader.readAsDataURL(file);
 		reader.onloadend = () => {
 			setImagePreview(reader.result);
 		};
 	}, []);
 
+	// 파일 불러오기
 	const onUploadImageButtonClick = useCallback(() => {
 		imageRef.current.click();
 	}, []);
